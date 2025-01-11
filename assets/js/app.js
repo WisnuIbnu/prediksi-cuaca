4f7dfc2a3c21129b1355451478f6ec844121333a
WisnuIbnu <wisnuiben21@gmail.com>
2025-01-11 15:26:39 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -422,8 +422,7 @@ export const updateWeather = function (lat, lon) {
                 </div>
 
                 <p class="label-1">${date.getDate()} ${module.monthNames[date.getUTCMonth()]}</p>
-
-                <p class="label-1">${module.weekDayNames[date.getUTCDay()]}</p>
+                <p class="label-1">${module.weekDayNames[(date.getUTCDay() + 1) % 7]}</p>
             `;
 
             forecastSection.querySelector("[data-forecast-list]").appendChild(li);
